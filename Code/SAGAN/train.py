becb88f191183aeb882fb898464403b1fe6f6665
tangolin <huangsiteng@msn.com>
2024-01-29 16:25:03 +0800

    Add lines to check progress and debug

--- Code/SAGAN/train.py
+++ Code/SAGAN/train.py
@@ -11,7 +11,11 @@ from torchvision.utils import save_image
 
 def train(config, device):
     # Define a dataset
-    dataset = ImageFolder(config.train_data_dir, transform=CustomTransform)
+    dataset = ImageFolder(config.train_data_dir, transform=CustomTransform())
+    print(
+        f"Dataset created from {config.train_data_dir}. {len(dataset)} images in total."
+        + f" The mapping is {dataset.class_to_idx}."
+    )
 
     # Define a dataloader
     dataloader = DataLoader(
@@ -20,25 +24,27 @@ def train(config, device):
         shuffle=True,
         num_workers=config.num_workers,
         drop_last=True,
+        pin_memory=True,
     )
-    steps_per_epoch = len(dataloader)
-    save_step = int(config.save_step * steps_per_epoch)
     data_iter = iter(dataloader)
 
     # Create the validation vector
     val_z = torch.randn(config.batch_size, config.z_dim).to(device)
+    val_lab = torch.randint(0, 5, size=(config.batch_size,)).to(device)
+    print(f"Validation noise vector created with shape {tuple(val_z.shape)}.")
 
     # Create the model and the optimizer
     gan_generator = Generator(
         in_features=config.z_dim,
         g_feature_dim=config.feature_dim,
         num_classes=config.num_classes,
-    )
+    ).to(device)
     gan_discriminator = Discriminator(
         config.in_channels,
         d_feature_dim=config.feature_dim,  # Should be same as config.g_feature_dim
         num_classes=config.num_classes,
-    )
+    ).to(device)
+    print("GAN model initialised.")
 
     g_optimizer = torch.optim.Adam(
         filter(lambda p: p.requires_grad, gan_generator.parameters()),
@@ -50,6 +56,7 @@ def train(config, device):
         config.d_lr,
         betas=[config.optim_beta_1, config.optim_beta_2],
     )
+    print("Optimizers initialised.")
 
     # Define the current step and loss
     cur_step = 0
@@ -57,6 +64,7 @@ def train(config, device):
     # Continue training a model
     if config.resume_training:
         assert config.ckpt_path is not None
+        print(f"Resuming training from {config.ckpt_path}.")
         checkpoint = torch.load(config.ckpt_path)
         gan_generator.load_state_dict(checkpoint["gan_generator_state_dict"])
         gan_discriminator.load_state_dict(checkpoint["gan_discriminator_state_dict"])
@@ -64,9 +72,11 @@ def train(config, device):
         d_optimizer.load_state_dict(checkpoint["d_optimizer_state_dict"])
         cur_step = checkpoint["step"] + 1
 
+    print(f"Starting training from step {cur_step}.")
+    gan_generator.train()
+    gan_discriminator.train()
+
     for step in range(cur_step, config.total_steps):
-        gan_generator.train()
-        gan_discriminator.train()
         g_optimizer.zero_grad()
         d_optimizer.zero_grad()
 
@@ -106,30 +116,32 @@ def train(config, device):
             labels,
         )
 
-        with torch.no_grad():
-            g_out = gan_discriminator(fake_images, labels)
+        # Discriminator accumulates gradient here but do not step d_optimizer
+        g_out = gan_discriminator(fake_images, labels)
         g_loss = -g_out.mean()
 
         g_loss.backward()
         g_optimizer.step()
 
         # Print out log info
-        if (step + 1) % config.log_step == 0:
+        if (step + 1) % config.log_n_step == 0:
             print(
-                f"G_step [{step + 1}/{config.total_step}], D_step[{(step + 1)}/{config.total_step}],"
-                + f"d_out_real: {d_real_loss.item():.4f}, d_out_real: {g_loss.item():.4f}"
+                f"G_step [{step + 1}/{config.total_steps}], D_step[{(step + 1)}/{config.total_steps}],"
+                + f" d_real_loss: {d_real_loss.item():.4f}, d_fake_loss: {d_fake_loss.item():.4f},"
+                + f" g_loss: {g_loss.item():.4f}"
             )
 
         # Sample images
         if (step + 1) % config.sample_step == 0:
-            fake_images = gan_generator(val_z)
+            with torch.no_grad():
+                fake_images = gan_generator(val_z, val_lab)
             save_image(
-                fake_images.detach() * torch.Tensor(0.5) + torch.Tensor(0.5),
-                os.path.join(config.sample_img_path, "{}_fake.png".format(step + 1)),
+                fake_images * torch.Tensor([0.5]) + torch.Tensor([0.5]),
+                os.path.join(config.sample_img_path, f"{step + 1}_fake.png"),
             )
 
         # Save checkpoint
-        if (step + 1) % save_step == 0:
+        if (step + 1) % config.save_step == 0:
             torch.save(
                 {
                     "step": cur_step,
@@ -138,5 +150,5 @@ def train(config, device):
                     "g_optimizer_state_dict": g_optimizer.state_dict(),
                     "d_optimizer_state_dict": d_optimizer.state_dict(),
                 },
-                config.model_save_path,
+                os.path.join(config.model_save_path, f"step_{step + 1}.pt"),
             )
