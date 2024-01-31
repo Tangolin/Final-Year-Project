46b9df0f76b3521bb2d27dd5a38698ca669032d0
tangolin <huangsiteng@msn.com>
2024-01-31 16:39:10 +0800

    Port in D and G to test training scripts

---
+++ Code/SAGAN/discriminator_test.py
@@ -0,0 +1,125 @@
+# from spectral import spectral_norm
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.nn.utils.parametrizations import spectral_norm
+
+
+class Self_Attn(nn.Module):
+    """Self attention Layer"""
+
+    def __init__(self, in_dim, activation):
+        super(Self_Attn, self).__init__()
+        self.chanel_in = in_dim
+        self.activation = activation
+
+        self.query_conv = nn.Conv2d(
+            in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1
+        )
+        self.key_conv = nn.Conv2d(
+            in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1
+        )
+        self.value_conv = nn.Conv2d(
+            in_channels=in_dim, out_channels=in_dim, kernel_size=1
+        )
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+        self.softmax = nn.Softmax(dim=-1)  #
+
+    def forward(self, x):
+        """
+        inputs :
+            x : input feature maps( B X C X W X H)
+        returns :
+            out : self attention value + input feature
+            attention: B X N X N (N is Width*Height)
+        """
+        m_batchsize, C, width, height = x.size()
+        proj_query = (
+            self.query_conv(x).view(m_batchsize, -1, width * height).permute(0, 2, 1)
+        )  # B X CX(N)
+        proj_key = self.key_conv(x).view(
+            m_batchsize, -1, width * height
+        )  # B X C x (*W*H)
+        energy = torch.bmm(proj_query, proj_key)  # transpose check
+        attention = self.softmax(energy)  # BX (N) X (N)
+        proj_value = self.value_conv(x).view(
+            m_batchsize, -1, width * height
+        )  # B X C X N
+
+        out = torch.bmm(proj_value, attention.permute(0, 2, 1))
+        out = out.view(m_batchsize, C, width, height)
+
+        out = self.gamma * out + x
+        return out, attention
+
+
+class Discriminator_Test(nn.Module):
+    """Discriminator, Auxiliary Classifier."""
+
+    def __init__(self, batch_size=64, image_size=64, conv_dim=64):
+        super().__init__()
+        self.imsize = image_size
+        layer1 = []
+        layer2 = []
+        layer3 = []
+        last = []
+
+        layer1.append(spectral_norm(nn.Conv2d(3, conv_dim, 4, 2, 1)))
+        layer1.append(nn.LeakyReLU(0.1))
+
+        curr_dim = conv_dim
+
+        layer2.append(spectral_norm(nn.Conv2d(curr_dim, curr_dim * 2, 4, 2, 1)))
+        layer2.append(nn.LeakyReLU(0.1))
+        curr_dim = curr_dim * 2
+
+        layer3.append(spectral_norm(nn.Conv2d(curr_dim, curr_dim * 2, 4, 2, 1)))
+        layer3.append(nn.LeakyReLU(0.1))
+        curr_dim = curr_dim * 2
+
+        if self.imsize >= 64:
+            layer4 = []
+            layer4.append(spectral_norm(nn.Conv2d(curr_dim, curr_dim * 2, 4, 2, 1)))
+            layer4.append(nn.LeakyReLU(0.1))
+            self.l4 = nn.Sequential(*layer4)
+            curr_dim = curr_dim * 2
+
+        if self.imsize >= 128:
+            layer5 = []
+            layer5.append(spectral_norm(nn.Conv2d(curr_dim, curr_dim * 2, 4, 2, 1)))
+            layer5.append(nn.LeakyReLU(0.1))
+            self.l5 = nn.Sequential(*layer5)
+            curr_dim = curr_dim * 2
+
+        self.l1 = nn.Sequential(*layer1)
+        self.l2 = nn.Sequential(*layer2)
+        self.l3 = nn.Sequential(*layer3)
+
+        last.append(nn.Conv2d(curr_dim, 1, 4))
+        self.last = nn.Sequential(*last)
+
+        self.attn1 = Self_Attn(256, "relu")
+        self.attn2 = Self_Attn(512, "relu")
+
+    def forward(self, x):
+        # print("disc")
+        out = self.l1(x)
+        # print("l1", out.shape)
+        out = self.l2(out)
+        # print("l2", out.shape)
+        out = self.l3(out)
+        # print("l3", out.shape)
+        out, p1 = self.attn1(out)
+        # print("a1", out.shape)
+        if self.imsize >= 64:
+            out = self.l4(out)
+            # print("l4", out.shape)
+            out, p2 = self.attn2(out)
+            # print("a2", out.shape)
+            if self.imsize >= 128:
+                out = self.l5(out)
+                # print("l5", out.shape)
+        out = self.last(out)
+
+        return out.squeeze(), p1, p2
---
+++ Code/SAGAN/train_test.py
@@ -0,0 +1,192 @@
+import datetime
+import os
+import time
+
+import torch
+from discriminator_test import Discriminator_Test
+from discriminator_trial import Discriminator
+from generator import Generator
+from generator_test import Generator_Test
+from ops import ToGray, denorm
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+from torchvision.utils import save_image
+
+
+def train(config, device):
+    # Define a dataset
+    t = transforms.Compose(
+        [
+            transforms.Resize(config.imsize),
+            transforms.ToTensor(),
+            ToGray(),
+            transforms.Normalize(mean=[0.5], std=[0.5]),
+        ]
+    )
+
+    dataset = ImageFolder(config.train_data_dir, transform=t)
+
+    print(
+        f"Dataset created from {config.train_data_dir}. {len(dataset)} images in total."
+        + f" The mapping is {dataset.class_to_idx}.",
+        flush=True,
+    )
+
+    # Define a dataloader
+    dataloader = DataLoader(
+        dataset,
+        batch_size=config.batch_size,
+        shuffle=True,
+        num_workers=config.num_workers,
+        drop_last=True,
+        pin_memory=True,
+    )
+    data_iter = iter(dataloader)
+
+    # Create the validation vector
+    val_z = torch.randn(config.batch_size, config.z_dim).to(device)
+    val_lab = torch.randint(0, 5, size=(config.batch_size,)).to(device)
+    print(
+        f"Validation noise vector created with shape {tuple(val_z.shape)}.", flush=True
+    )
+
+    # Create the model and the optimizer
+    # gan_generator = Generator_Test(
+    #     z_dim=config.z_dim, conv_dim=config.feature_dim, image_size=config.imsize
+    # ).to(device)
+    # gan_discriminator = Discriminator_Test(
+    #     conv_dim=config.feature_dim,  # Should be same as config.g_feature_dim
+    #     image_size=config.imsize,
+    # ).to(device)
+    gan_generator = Generator(
+        in_features=config.z_dim,
+        g_feature_dim=config.feature_dim,
+        num_classes=config.num_classes,
+        out_channels=config.num_channels,
+    ).to(device)
+    gan_discriminator = Discriminator(
+        in_channels=config.num_channels,
+        d_feature_dim=config.feature_dim,  # Should be same as config.g_feature_dim
+        num_classes=config.num_classes,
+    ).to(device)
+    print(gan_generator)
+    print(gan_discriminator)
+    print("GAN model initialised.", flush=True)
+
+    g_optimizer = torch.optim.Adam(
+        filter(lambda p: p.requires_grad, gan_generator.parameters()),
+        config.g_lr,
+        betas=[config.optim_beta_1, config.optim_beta_2],
+    )
+    d_optimizer = torch.optim.Adam(
+        filter(lambda p: p.requires_grad, gan_discriminator.parameters()),
+        config.d_lr,
+        betas=[config.optim_beta_1, config.optim_beta_2],
+    )
+    print("Optimizers initialised.", flush=True)
+
+    # Define the current step and loss
+    cur_step = 0
+
+    # Continue training a model
+    if config.resume_training:
+        assert config.ckpt_path is not None
+        print(f"Resuming training from {config.ckpt_path}.", flush=True)
+        checkpoint = torch.load(config.ckpt_path)
+        gan_generator.load_state_dict(checkpoint["gan_generator_state_dict"])
+        gan_discriminator.load_state_dict(checkpoint["gan_discriminator_state_dict"])
+        g_optimizer.load_state_dict(checkpoint["g_optimizer_state_dict"])
+        d_optimizer.load_state_dict(checkpoint["d_optimizer_state_dict"])
+        cur_step = checkpoint["step"] + 1
+
+    print(f"Starting training from step {cur_step}.", flush=True)
+    gan_generator.train()
+    gan_discriminator.train()
+
+    start_time = time.time()
+    for step in range(cur_step, config.total_steps):
+        g_optimizer.zero_grad()
+        d_optimizer.zero_grad()
+        gan_discriminator.zero_grad()
+
+        try:
+            real_images, labels = next(data_iter)
+        except:
+            data_iter = iter(dataloader)
+            real_images, labels = next(data_iter)
+
+        # Prepare the real and fake images
+        real_images, labels = real_images.to(device), labels.to(device)
+        if step == 0:
+            print(f"Images has shape: {tuple(real_images.shape)}.")
+            print(f"Labels has shape: {tuple(labels.shape)}.")
+            print(labels)
+
+        with torch.no_grad():
+            noise_vector = torch.randn((config.batch_size, config.z_dim)).to(device)
+            fake_images = gan_generator(
+                # Take in the fake dimensions and labels
+                noise_vector,
+                labels,
+            )
+
+        d_real_out = gan_discriminator(real_images, labels)
+        d_fake_out = gan_discriminator(fake_images, labels)
+
+        # Calculate the 2 losses separately
+        d_real_loss = torch.nn.ReLU()(1 - d_real_out).mean()
+        d_fake_loss = torch.nn.ReLU()(1 + d_fake_out).mean()
+        d_loss = d_real_loss + d_fake_loss
+
+        # Do a backward pass on generator first
+        d_loss.backward()
+        d_optimizer.step()
+
+        noise_vector = torch.randn((config.batch_size, config.z_dim)).to(device)
+        fake_images = gan_generator(noise_vector, labels)
+
+        # Discriminator accumulates gradient here but do not step d_optimizer
+        g_out = gan_discriminator(fake_images, labels)
+        g_loss = -g_out.mean()
+
+        g_loss.backward()
+        g_optimizer.step()
+
+        # Print out log info
+        if (step + 1) % config.log_n_step == 0:
+            elapsed = time.time() - start_time
+            elapsed = str(datetime.timedelta(seconds=elapsed))
+            print(
+                f"Elapsed [{elapsed}]"
+                + f" G_step [{step + 1}/{config.total_steps}], D_step[{(step + 1)}/{config.total_steps}],"
+                + f" d_real_loss: {d_real_loss.item():.4f}, d_fake_loss: {d_fake_loss.item():.4f},"
+                + f" g_loss: {g_loss.item():.4f}",
+                flush=True,
+            )
+
+        # Sample images
+        if (step + 1) % config.sample_step == 0:
+            with torch.no_grad():
+                fake_images = gan_generator(val_z, labels)
+            save_image(
+                denorm(real_images.detach()),
+                os.path.join(config.sample_img_path, f"{step + 1}_real.png"),
+            )
+            save_image(
+                denorm(fake_images.detach()),
+                os.path.join(config.sample_img_path, f"{step + 1}_fake.png"),
+            )
+
+        # Save checkpoint
+        if (step + 1) % config.save_step == 0:
+            torch.save(
+                {
+                    "step": cur_step,
+                    "gan_generator_state_dict": gan_generator.state_dict(),
+                    "gan_discriminator_state_dict": gan_discriminator.state_dict(),
+                    "g_optimizer_state_dict": g_optimizer.state_dict(),
+                    "d_optimizer_state_dict": d_optimizer.state_dict(),
+                },
+                os.path.join(config.model_save_path, f"step_{step + 1}.pt"),
+            )
