d667cba895056b86f3e5fa3689fed9667272ab5e
tangolin <huangsiteng@msn.com>
2024-02-05 10:00:18 +0800

    Combine evaluation and extraction functions

--- code/fid_is_backbone/extract_feat.py
+++ code/fid_is_backbone/extract_feat.py
@@ -1,69 +1,94 @@
+import os
+
 import numpy as np
 import torch
+from torch.backends import cudnn
 from torch.utils.data import DataLoader
 from torcheval.metrics import MulticlassAccuracy
-from torchvision import datasets, models, transforms
+from torchvision import datasets
+from utils import calc_fid_score, get_resnet_model, preprocess
 
-model_dir = "./models/best.pt"
+torch.manual_seed(42)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+cudnn.deterministic = True
+cudnn.benchmark = False
+
 data_dir = "./data/sample/"
+output_dir = "./output"
 num_classes = 5
 
-# Set device (CPU or GPU) and load model
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
-model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
-checkpoint = torch.load(model_dir)
-model.load_state_dict(checkpoint["model_state_dict"])
-model_feature_extractor = torch.nn.Sequential(*(list(model.children())[:-1]))
-model_label_predictor = list(model.children())[-1]
-model_feature_extractor.to(device)
-model_label_predictor.to(device)
-del model
+for i in [8, 16, 32, 64, 128, 512]:
+    print("=" * 60)
+    num_neurons = i
+    model_dir = f"./models/gait_resnet_{num_neurons}.pt"
+    print(f"Trial with {num_neurons} neurons.")
 
-preprocess = transforms.Compose(
-    [
-        transforms.Resize(128),  # Resize to 128 to emulate actual process
-        transforms.Resize(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ]
-)
+    # Load the relevant model
+    model, label_predictor = get_resnet_model(
+        num_neurons=num_neurons, num_classes=num_classes, ckpt=model_dir, split=True
+    )
+    model.to(device)
+    label_predictor.to(device)
 
-# Set up the dataset
-sample_dataset = datasets.ImageFolder(root=data_dir, transform=preprocess)
-sample_loader = DataLoader(
-    sample_dataset,
-    batch_size=128,
-    shuffle=True,
-    num_workers=4,
-    pin_memory=True,
-    drop_last=False,
-)
+    # Set up the dataset
+    sample_dataset = datasets.ImageFolder(root=data_dir, transform=preprocess)
+    sample_loader = DataLoader(
+        sample_dataset,
+        batch_size=128,
+        shuffle=True,
+        num_workers=4,
+        pin_memory=True,
+        drop_last=False,
+    )
 
-features = []
-accuracy = MulticlassAccuracy()
+    # 'diplegic': 0, 'hemiplegic': 1, 'neuropathic': 2, 'normal': 3, 'parkinsonian': 4
+    print("The mapping is", sample_dataset.class_to_idx)
 
-with torch.no_grad():
-    for idx, (imgs, labels) in enumerate(sample_loader):
-        imgs, labels = imgs.to(device), labels.to(device)
-        out = model_feature_extractor(imgs)
-        output = torch.flatten(out, 1)
-        features.append(output)
+    features = []
+    labels = []
+    accuracy = MulticlassAccuracy()
 
-        logits = model_label_predictor(output)
-        _, pred = torch.max(logits, 1)
-        accuracy.update(pred, labels)
+    with torch.no_grad():
+        for idx, (img, label) in enumerate(sample_loader):
+            img, label = img.to(device), label.to(device)
+            out_features = model(img)
+            features.append(out_features)
+            labels.append(label)
 
-# Checking the accuracy on the samples for fun
-print("Accuracy", accuracy.compute().item())
+            logits = label_predictor(out_features)
+            _, pred = torch.max(logits, 1)
+            accuracy.update(pred, label)
 
-# Save the tensors in case of future needs
-features = torch.cat(features, dim=0).to(torch.device("cpu"))
-torch.save(features, "features.pt")
+    # Checking the accuracy on the samples for fun
+    print(
+        f"The accuracy of the model is {accuracy.compute().item():.2d}.",
+    )
 
-# Get statistics for FID
-features = features.numpy()
-mu = np.mean(features, axis=0)
-sigma = np.cov(features, rowvar=False)
-np.savez("feature_stats.npz", mu=mu, sigma=sigma)
-print(f"The features have mean shape {mu.shape} and covariance shape {sigma.shape}.")
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    # Save the tensors in case of future needs
+    features = torch.cat(features, dim=0).cpu().numpy()
+    labels = torch.cat(labels, dim=0).cpu().numpy()
+    torch.save(features, f"./output/features_{num_neurons}.pt")
+
+    # Get statistics for FID
+    features = features.numpy()
+    mu = np.mean(features, axis=0)
+    sigma = np.cov(features, rowvar=False)
+    print(mu, sigma)
+    np.savez(f"./output/feature_stats_{num_neurons}.npz", mu=mu, sigma=sigma)
+    print(
+        f"The features have mean shape {mu.shape} and covariance shape {sigma.shape}."
+    )
+
+    # Calculate the matrix of FID scores
+    matrix = [[0] * 5 for _ in range(5)]
+    for i in range(5):
+        for j in range(5):
+            out1 = features[np.where(labels == i)]
+            out2 = features[np.where(labels == j)]
+            matrix[i][j] = calc_fid_score(out1, out2)
+
+    print(*matrix, sep="\n")
