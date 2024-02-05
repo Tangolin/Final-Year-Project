f6533317344b512bfe46f49cc91ffbe5bd8d67d4
tangolin <huangsiteng@msn.com>
2024-02-05 09:59:44 +0800

    Fix bug where validation is not done correctly

--- code/fid_is_backbone/train.py
+++ code/fid_is_backbone/train.py
@@ -3,31 +3,28 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torcheval.metrics import MulticlassAccuracy, MulticlassF1Score
-from torchvision import datasets, models, transforms
+from torchvision import datasets
+from utils import get_resnet_model, preprocess
 
 torch.backends.cudnn.benchmark = True
 torch.manual_seed(42)
-
-# Set device (CPU or GPU) and create model
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
 
 # Declare the parameters for training
 num_classes = 5
 batch_size = 128
 learning_rate = 0.1
-num_epochs = 100
+num_epochs = 50
+num_neurons = 8
 best_eval = float("inf")
+best_acc = float("inf")
+best_f1 = float("inf")
 
-# Data loading and preprocessing
-preprocess = transforms.Compose(
-    [
-        transforms.Resize(128),  # Resize to 128 to emulate actual process
-        transforms.Resize(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ]
-)
+# Set device (CPU or GPU) and create model
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = get_resnet_model(num_neurons, num_classes)
+model.to(device)
+print("Model initialised.")
+print(model)
 
 # Create the train and test dataset
 train_dataset = datasets.ImageFolder(root="./data/train", transform=preprocess)
@@ -41,7 +38,7 @@ train_loader = DataLoader(
 )
 val_dataset = datasets.ImageFolder(root="./data/val", transform=preprocess)
 val_loader = DataLoader(
-    train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True
+    val_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True
 )
 print("Training and validation datasets created.")
 
@@ -65,20 +62,13 @@ class EarlyStopper:
         return False
 
 
-# Customise model by replacing head
-in_features = model.fc.in_features
-model.fc = nn.Linear(in_features, num_classes)
-model.to(device)
-print("Model initialised.")
-print(model)
-
 # Declare the helpers for the training
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate, betas=(0.8, 0.999))
 scheduler = optim.lr_scheduler.CosineAnnealingLR(
     optimizer, T_max=num_epochs * int(len(train_dataset) / batch_size)
 )
-early_stopper = EarlyStopper(patience=10, min_delta=1e-4)
+# early_stopper = EarlyStopper(patience=10, min_delta=1e-4)
 
 for epoch in range(num_epochs):
     # Train
@@ -89,6 +79,7 @@ for epoch in range(num_epochs):
         optimizer.zero_grad()
 
         data, target = data.to(device), target.to(device)
+
         if batch_idx == 0 and epoch == 0:
             print("Data has shape", data.shape)
             print("Target has shape", target.shape)
@@ -124,10 +115,6 @@ for epoch in range(num_epochs):
             _, pred = torch.max(output, 1)
             accuracy.update(pred, labels)
             f1_score.update(pred, labels)
-            # print(output)
-            # print(pred)
-            # print(labels)
-            # print(criterion(output, labels))
 
     acc_value = accuracy.compute().item()
     f1_value = f1_score.compute().item()
@@ -135,7 +122,7 @@ for epoch in range(num_epochs):
     if val_loss < best_eval:
         # Save the model if f1 is best
         print(
-            f"Saving model to best.pt, current val-loss: {val_loss}, prev best: {best_eval}"
+            f"Saving model to gait_resnet_{num_neurons}.pt, current val-loss: {val_loss}, prev best: {best_eval}"
         )
         torch.save(
             {
@@ -144,16 +131,19 @@ for epoch in range(num_epochs):
                 "model_state_dict": model.state_dict(),
                 "optimizer_state_dict": optimizer.state_dict(),
             },
-            "./models/best.pt",
+            f"./models/gait_resnet_{num_neurons}.pt",
         )
         best_eval = val_loss
+        best_acc = acc_value
+        best_f1 = f1_value
 
     print(
         f"Epoch {epoch+1}/{num_epochs}, Loss: {val_loss}, Accuracy: {acc_value}, F1: {f1_value}."
     )
 
-    if early_stopper.early_stop(val_loss):
-        print(f"Early stopping. Final epoch: {epoch}.")
-        break
+    # if early_stopper.early_stop(val_loss):
+    #     print(f"Early stopping. Final epoch: {epoch}.")
+    #     print(f"Loss: {best_eval}, Accuracy: {best_acc}, F1: {best_f1}.")
+    #     break
 
 print("Training completed.")
