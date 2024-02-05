233c4d8df73d7a653fac7d7d1e573a1e1b841d72
tangolin <huangsiteng@msn.com>
2024-02-05 10:07:06 +0800

    Make trial discriminator official

--- code/self_attn_gan/discriminator.py
+++ code/self_attn_gan/discriminator.py
@@ -2,25 +2,24 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as init
-from ops import AttentionBlock
+from ops import AttentionBlock, AttentionBlockNoPool
 from torch.nn.utils.parametrizations import spectral_norm
 
 
 class DiscriminatorBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, downsample=True):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.downsample = downsample
         self.out_channels = out_channels
 
         self.main_bloc = nn.Sequential(
-            nn.ReLU(),
             spectral_norm(
                 nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
             ),
-            nn.ReLU(),
+            nn.LeakyReLU(0.1),
             spectral_norm(
                 nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
             ),
+            nn.LeakyReLU(0.1),
         )
 
         self.skip_conv = spectral_norm(
@@ -40,30 +39,73 @@ class DiscriminatorBlock(nn.Module):
 
     def forward(self, x):
         x_0 = x
+
         x = self.main_bloc(x)
-
-        if self.downsample:
-            x = F.avg_pool2d(x, kernel_size=2, stride=2, padding=0)
-
-        if self.downsample or x_0.shape[1] != self.out_channels:
-            x_0 = self.skip_conv(x_0)
-            if self.downsample:
-                x_0 = F.avg_pool2d(x_0, kernel_size=2, stride=2, padding=0)
+        x_0 = self.skip_conv(x_0)
+        x = F.avg_pool2d(x, kernel_size=2, stride=2, padding=0)
+        x_0 = F.avg_pool2d(x_0, kernel_size=2, stride=2, padding=0)
 
         return x_0 + x
 
 
-class DiscriminatorOptimBlock(nn.Module):
-    def __init__(self, in_channels, out_channels):
+class DiscriminatorBlockNoPool(nn.Module):
+    def __init__(self, in_channels, out_channels, decay_rate=0.999):
         super().__init__()
+        self.out_channels = out_channels
+
+        self.main_bloc = nn.Sequential(
+            spectral_norm(
+                nn.Conv2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1)
+            ),
+            nn.BatchNorm2d(out_channels, momentum=decay_rate, eps=1e-5),
+            nn.LeakyReLU(0.1),
+            spectral_norm(
+                nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+            ),
+            nn.BatchNorm2d(out_channels, momentum=decay_rate, eps=1e-5),
+            nn.LeakyReLU(0.1),
+        )
+
+        self.skip_conv = spectral_norm(
+            nn.Conv2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1)
+        )
+
+        self.initialise_weights()
+
+    def initialise_weights(self):
+        for layer in self.main_bloc.children():
+            if isinstance(layer, nn.Conv2d):
+                init.xavier_uniform_(layer.weight)
+                init.constant_(layer.bias, 0)
+
+        init.xavier_uniform_(self.skip_conv.weight)
+        init.constant_(self.skip_conv.bias, 0)
+
+    def forward(self, x):
+        x_0 = x
+
+        x = self.main_bloc(x)
+        x_0 = self.skip_conv(x_0)
+
+        return x_0 + x
+
+
+class DiscriminatorLastBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, decay_rate=0.999):
+        super().__init__()
+        self.out_channels = out_channels
+
         self.main_bloc = nn.Sequential(
             spectral_norm(
                 nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
             ),
-            nn.ReLU(),
+            nn.BatchNorm2d(out_channels, momentum=decay_rate, eps=1e-5),
+            nn.LeakyReLU(0.1),
             spectral_norm(
                 nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
             ),
+            nn.BatchNorm2d(out_channels, momentum=decay_rate, eps=1e-5),
+            nn.LeakyReLU(0.1),
         )
 
         self.skip_conv = spectral_norm(
@@ -83,13 +125,9 @@ class DiscriminatorOptimBlock(nn.Module):
 
     def forward(self, x):
         x_0 = x
-
         x = self.main_bloc(x)
-        x = F.avg_pool2d(x, kernel_size=2, stride=2, padding=0)
-        x_0 = F.avg_pool2d(x_0, kernel_size=2, stride=2, padding=0)
-        x_0 = self.skip_conv(x_0)
 
-        return x + x_0
+        return x_0 + x
 
 
 class Discriminator(nn.Module):
@@ -98,18 +136,15 @@ class Discriminator(nn.Module):
 
         self.sequential = nn.Sequential(
             # Img size 64 * 64
-            DiscriminatorOptimBlock(in_channels, d_feature_dim),
-            DiscriminatorBlock(d_feature_dim, d_feature_dim * 2),
+            DiscriminatorBlockNoPool(in_channels, d_feature_dim),
+            DiscriminatorBlockNoPool(d_feature_dim, d_feature_dim * 2),
             # Img size 32 * 32
-            AttentionBlock(d_feature_dim * 2),
-            DiscriminatorBlock(d_feature_dim * 2, d_feature_dim * 4),
-            DiscriminatorBlock(d_feature_dim * 4, d_feature_dim * 8),
+            AttentionBlockNoPool(d_feature_dim * 2),
+            DiscriminatorBlockNoPool(d_feature_dim * 2, d_feature_dim * 4),
+            DiscriminatorBlockNoPool(d_feature_dim * 4, d_feature_dim * 8),
             # Img size 4 * 4
-            DiscriminatorBlock(d_feature_dim * 8, d_feature_dim * 16),
-            DiscriminatorBlock(
-                d_feature_dim * 16, d_feature_dim * 16, downsample=False
-            ),
-            nn.ReLU(),
+            DiscriminatorBlockNoPool(d_feature_dim * 8, d_feature_dim * 16),
+            DiscriminatorLastBlock(d_feature_dim * 16, d_feature_dim * 16),
         )
 
         self.sn_linear = spectral_norm(nn.Linear(d_feature_dim * 16, 1))
