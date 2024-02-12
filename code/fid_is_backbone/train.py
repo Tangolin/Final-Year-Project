2b9cb8da4b27fcdbd915f9bf595f5205497aa83b
tangolin <huangsiteng@msn.com>
2024-02-12 22:54:03 +0800

    Combine into training loop

--- code/fid_is_backbone/train.py
+++ code/fid_is_backbone/train.py
@@ -14,33 +14,13 @@ num_classes = 5
 batch_size = 128
 learning_rate = 0.1
 num_epochs = 50
-num_neurons = 8
-best_eval = float("inf")
-best_acc = float("inf")
-best_f1 = float("inf")
 
 # Set device (CPU or GPU) and create model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = get_resnet_model(num_neurons, num_classes)
-model.to(device)
-print("Model initialised.")
-print(model)
 
 # Create the train and test dataset
-train_dataset = datasets.ImageFolder(root="./data/train", transform=preprocess)
-train_loader = DataLoader(
-    train_dataset,
-    batch_size=batch_size,
-    shuffle=True,
-    num_workers=4,
-    pin_memory=True,
-    drop_last=True,
-)
-val_dataset = datasets.ImageFolder(root="./data/val", transform=preprocess)
-val_loader = DataLoader(
-    val_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True
-)
-print("Training and validation datasets created.")
+train_dataset = datasets.ImageFolder(root="../data/train", transform=preprocess)
+val_dataset = datasets.ImageFolder(root="../data/val", transform=preprocess)
 
 
 # Creating an early stopping mechanism
@@ -62,88 +42,112 @@ class EarlyStopper:
         return False
 
 
-# Declare the helpers for the training
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=learning_rate, betas=(0.8, 0.999))
-scheduler = optim.lr_scheduler.CosineAnnealingLR(
-    optimizer, T_max=num_epochs * int(len(train_dataset) / batch_size)
-)
-# early_stopper = EarlyStopper(patience=10, min_delta=1e-4)
+for num_neurons in [8, 16, 32, 64, 128, 256, 512]:
+    print("*" * 100)
+    print(f"Begin training for Resnet with {num_neurons}.")
 
-for epoch in range(num_epochs):
-    # Train
-    print("=====================Training=====================")
-    model.train()
+    # Begin a new training loop
+    best_eval = float("inf")
+    best_acc = float("inf")
+    best_f1 = float("inf")
 
-    for batch_idx, (data, target) in enumerate(train_loader):
-        optimizer.zero_grad()
+    model = get_resnet_model(num_neurons, num_classes)
+    model.to(device)
 
-        data, target = data.to(device), target.to(device)
-
-        if batch_idx == 0 and epoch == 0:
-            print("Data has shape", data.shape)
-            print("Target has shape", target.shape)
-
-        # Forward pass
-        output = model(data)
-        loss = criterion(output, target)
-
-        # Backward pass and optimization
-        loss.backward()
-        optimizer.step()
-        scheduler.step()
-
-        if (batch_idx + 1) % 5 == 0 or batch_idx == len(train_loader) - 1:
-            print(
-                f"Epoch {epoch+1}/{num_epochs}, Step {batch_idx+1}/{len(train_loader)},"
-                + f" Loss: {loss.item()}."
-            )
-
-    # Test
-    print("====================Validation====================")
-    model.eval()  # Set the model to evaluation mode
-    accuracy = MulticlassAccuracy()
-    f1_score = MulticlassF1Score(num_classes=num_classes, average="macro")
-    val_loss = 0
-
-    with torch.no_grad():
-        for images, labels in val_loader:
-            images, labels = images.to(device), labels.to(device)
-
-            output = model(images)
-            val_loss += criterion(output, labels)
-            _, pred = torch.max(output, 1)
-            accuracy.update(pred, labels)
-            f1_score.update(pred, labels)
-
-    acc_value = accuracy.compute().item()
-    f1_value = f1_score.compute().item()
-
-    if val_loss < best_eval:
-        # Save the model if f1 is best
-        print(
-            f"Saving model to gait_resnet_{num_neurons}.pt, current val-loss: {val_loss}, prev best: {best_eval}"
-        )
-        torch.save(
-            {
-                "epoch": epoch,
-                "best_acc": f1_value,
-                "model_state_dict": model.state_dict(),
-                "optimizer_state_dict": optimizer.state_dict(),
-            },
-            f"./models/gait_resnet_{num_neurons}.pt",
-        )
-        best_eval = val_loss
-        best_acc = acc_value
-        best_f1 = f1_value
-
-    print(
-        f"Epoch {epoch+1}/{num_epochs}, Loss: {val_loss}, Accuracy: {acc_value}, F1: {f1_value}."
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=4,
+        pin_memory=True,
+        drop_last=True,
+    )
+    val_loader = DataLoader(
+        val_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True
     )
 
-    # if early_stopper.early_stop(val_loss):
-    #     print(f"Early stopping. Final epoch: {epoch}.")
-    #     print(f"Loss: {best_eval}, Accuracy: {best_acc}, F1: {best_f1}.")
-    #     break
+    # Declare the helpers for the training
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate, betas=(0.8, 0.999))
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(
+        optimizer, T_max=num_epochs * int(len(train_dataset) / batch_size)
+    )
+    early_stopper = EarlyStopper(patience=10, min_delta=1e-4)
 
-print("Training completed.")
+    for epoch in range(num_epochs):
+        # Train
+        print("=====================Training=====================")
+        model.train()
+
+        for batch_idx, (data, target) in enumerate(train_loader):
+            optimizer.zero_grad()
+
+            data, target = data.to(device), target.to(device)
+
+            if batch_idx == 0 and epoch == 0:
+                print("Data has shape", data.shape)
+                print("Target has shape", target.shape)
+
+            # Forward pass
+            output = model(data)
+            loss = criterion(output, target)
+
+            # Backward pass and optimization
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+
+            if (batch_idx + 1) % 5 == 0 or batch_idx == len(train_loader) - 1:
+                print(
+                    f"Epoch {epoch+1}/{num_epochs}, Step {batch_idx+1}/{len(train_loader)},"
+                    + f" Loss: {loss.item()}."
+                )
+
+        # Test
+        print("====================Validation====================")
+        model.eval()  # Set the model to evaluation mode
+        accuracy = MulticlassAccuracy()
+        f1_score = MulticlassF1Score(num_classes=num_classes, average="macro")
+        val_loss = 0
+
+        with torch.no_grad():
+            for images, labels in val_loader:
+                images, labels = images.to(device), labels.to(device)
+
+                output = model(images)
+                val_loss += criterion(output, labels)
+                _, pred = torch.max(output, 1)
+                accuracy.update(pred, labels)
+                f1_score.update(pred, labels)
+
+        acc_value = accuracy.compute().item()
+        f1_value = f1_score.compute().item()
+
+        if val_loss < best_eval:
+            # Save the model if f1 is best
+            print(
+                f"Saving model to gait_resnet_{num_neurons}.pt, current val-loss: {val_loss}, prev best: {best_eval}"
+            )
+            torch.save(
+                {
+                    "epoch": epoch,
+                    "best_acc": f1_value,
+                    "model_state_dict": model.state_dict(),
+                    "optimizer_state_dict": optimizer.state_dict(),
+                },
+                f"./models/gait_resnet_{num_neurons}.pt",
+            )
+            best_eval = val_loss
+            best_acc = acc_value
+            best_f1 = f1_value
+
+        print(
+            f"Epoch {epoch+1}/{num_epochs}, Loss: {val_loss}, Accuracy: {acc_value}, F1: {f1_value}."
+        )
+
+        if early_stopper.early_stop(val_loss):
+            print(f"Early stopping. Final epoch: {epoch}.")
+            print(f"Loss: {best_eval}, Accuracy: {best_acc}, F1: {best_f1}.")
+            break
+
+    print("Training completed.")
